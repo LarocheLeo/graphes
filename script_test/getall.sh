55d3e0149afc1e0272c67d6c21606190eb20b9fe
LarocheLeo <larocheleo@free.fr>
2025-11-03 15:17:43 +0100

    option en plus

--- script_test/getall.sh
+++ script_test/getall.sh
@@ -11,6 +11,8 @@ curl -L \
   -H "X-GitHub-Api-Version: 2022-11-28" \
    https://api.github.com/repos/LarocheLeo/graphes/actions/artifacts/$x/zip \
   --output artifact_$x.zip
+mkdir artifact_$x
+unzip -q "artifact_$x.zip" -d "artifact_$x"
 
 #tout les artéfacts 
 
@@ -22,4 +24,7 @@ curl -L \
 #      -H "X-GitHub-Api-Version: 2022-11-28" \
 #      https://api.github.com/repos/LarocheLeo/graphes/actions/artifacts/$ID/zip \
 #      --output "artifact-$ID.zip"
+#     mkdir artifact_$ID
+#     unzip -q "artifact_$ID.zip" -d "artifact_$ID"
 #done < liste-id.txt
+
