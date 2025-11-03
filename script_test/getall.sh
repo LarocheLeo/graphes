446f2da40c478787303ddcf0b3430405160465b4
LarocheLeo <larocheleo@free.fr>
2025-11-03 14:56:47 +0100

    correction de bug

--- script_test/getall.sh
+++ script_test/getall.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-source token.gh
-x=$(head -1 liste-id.txt)
 
-# télécharger l'artéfact le plus récent du fichier liste-id.txt
-echo "Téléchargement de l'artéfact récent : $x ..."
+#seulement le 1er 
+source token.gh
+x=4329254215
+
+echo "Téléchargement de l’artifact $x ..."
 curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $TOK" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/LarocheLeo/graphes/actions/artifacts/$ID/zip \
-  --output "artifact-$x.zip"
+   https://api.github.com/repos/LarocheLeo/graphes/actions/artifacts/$x/zip \
+  --output artifact_$x.zip
 
-
-# télécharger tout les artéfacts du fichier liste-id.txt
-#!/bin/bash
-#source token.gh
+#tout les artéfacts 
 
 #while read -r ID; do
 #    echo "Téléchargement de l’artifact $ID ..."
